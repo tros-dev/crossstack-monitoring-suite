4685dfa27eefe84e03fb539f47fb46d51948edff
rossirossi14 <tommieros@hotmail.com>
2025-06-12 10:43:53 +0200

    Update C# dashboard log message

--- csharp-dashboard/Program.cs
+++ csharp-dashboard/Program.cs
@@ -1,4 +1,6 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.MapGet("/", () => "Welcome to the C# Dashboard!");
+Console.WriteLine("Updated message!");
+
 app.Run();
